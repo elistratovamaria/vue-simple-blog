1e1e85d6be8ce551936230bfcad9885ed74d37f3
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-06-10 20:44:59 +0300

    Добавляет анимацию

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список постов</h3>
-    <post-item 
+    <transition-group name="post-list">
+      <post-item 
       v-for="post in posts"
       :post="post"
       :key="post.id"
       @remove="$emit('remove', post)"
     />
+    </transition-group>
   </div>
   <h2 v-else>Список постов пуст</h2>
 </template>
@@ -27,4 +29,18 @@ export default {
 </script>
 
 <style>
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+
+.post-list-move {
+  transition: transform 0.4s ease;
+}
 </style>
