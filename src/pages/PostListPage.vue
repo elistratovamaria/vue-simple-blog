647d49c77b31dc6f75369be54ed43e88c6d384de
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-06-11 18:53:26 +0300

    Добавляет директиву v-intersection

--- src/pages/PostListPage.vue
+++ src/pages/PostListPage.vue
@@ -13,7 +13,7 @@
       v-if="!isPostsLoading"
     />
     <div v-else>Идет загрузка...</div>
-    <div class="observer" ref="observer"></div>
+    <div class="observer" v-intersection="loadMorePosts"></div>
     <!-- <div class="page-wrapper">
       <div 
         class="page" 
@@ -111,17 +111,17 @@ export default {
   },
   mounted() {
     this.fetchPosts();
-    const options = {
-      rootMargin: "0px",
-      threshold: 1.0,
-    };
-    const callback = (entries, observer) => {
-      if (entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePosts();
-      }
-    };
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(this.$refs.observer);
+    // const options = {
+    //   rootMargin: "0px",
+    //   threshold: 1.0,
+    // };
+    // const callback = (entries, observer) => {
+    //   if (entries[0].isIntersecting && this.page < this.totalPages) {
+    //     this.loadMorePosts();
+    //   }
+    // };
+    // const observer = new IntersectionObserver(callback, options);
+    // observer.observe(this.$refs.observer);
   },
   computed: {
     sortedPosts() {
